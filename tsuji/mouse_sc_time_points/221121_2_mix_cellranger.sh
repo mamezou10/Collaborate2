426986c94256158d38fa48b7aed505052eac166f
mamezou10 <mamezou_10@yahoo.co.jp>
2022-11-21 22:40:26 +0900

    mix species

---
+++ tsuji/mouse_sc_time_points/221121_2_mix_cellranger.sh
@@ -0,0 +1,26 @@
+
+
+
+## mix cellranger
+cellranger count --id=tsuji_day4 \
+   --fastqs=/mnt/Daisy/tsuji_sc_mouse/F4710_220927_122647_fastq \
+   --sample=CMT167_1G2_D4_5DE \
+   --transcriptome=/mnt/Daisy/ref_cellranger_human_and_mouse/refdata-gex-GRCh38-and-mm10-2020-A
+
+
+cellranger count --id=tsuji_day7 \
+   --fastqs=/mnt/Daisy/tsuji_sc_mouse/F4710_220927_122647_fastq \
+   --sample=CMT167_1G2_D7_5DE \
+   --transcriptome=/mnt/Daisy/ref_cellranger_human_and_mouse/refdata-gex-GRCh38-and-mm10-2020-A
+
+cellranger count --id=tsuji_day10 \
+   --fastqs=/mnt/Daisy/tsuji_sc_mouse/F4710_220927_122647_fastq \
+   --sample=CMT167_1G2_D10_5DE \
+   --transcriptome=/mnt/Daisy/ref_cellranger_human_and_mouse/refdata-gex-GRCh38-and-mm10-2020-A
+
+cellranger count --id=tsuji_KO_day7 \
+   --fastqs=/mnt/Daisy/tsuji_sc_mouse/F4710_220927_122647_fastq \
+   --sample=CMT167_DK1_D7_5DE \
+   --transcriptome=/mnt/Daisy/ref_cellranger_human_and_mouse/refdata-gex-GRCh38-and-mm10-2020-A
+
+
